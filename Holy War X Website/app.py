894dc0a4c7ce541a1355ea992bfb348275d83d70
Daniel Morais <daniel13042008@gmail.com>
2025-06-30 10:28:23 +0100

    Messed Stuff Up

--- Holy War X Website/app.py
+++ Holy War X Website/app.py
@@ -5,21 +5,7 @@ from flask import Flask, url_for, redirect, render_template, request, session
 
 load_dotenv("key.env")
 
-app = Flask(__name__)
-app.secret_key = os.getenv("key")
-
-@app.route("/")
-@app.route("/main")
-def main():
-    return render_template("main.html")
-
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
+app = Flasml")
 
 if __name__ == "__main__":
     app.run()
