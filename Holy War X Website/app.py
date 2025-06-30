30023c47457240fdafe998a2580142cd102bab09
Daniel Morais <daniel13042008@gmail.com>
2025-06-30 10:32:43 +0100

    Fixed

--- Holy War X Website/app.py
+++ Holy War X Website/app.py
@@ -5,9 +5,23 @@ from flask import Flask, url_for, redirect, render_template, request, session
 
 load_dotenv("key.env")
 
-app = Flasml")
+app = Flask(__name__)
+app.secret_key = os.getenv("key")
+
+@app.route("/")
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run()
 
-#Test
+#Test - This is fine now
