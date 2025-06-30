b860a633625e357f0a76a1c7cfa2c42b0e086388
Kattbird <emirnedzhmiev@gmail.com>
2025-06-30 11:57:26 +0100

    Added a comments table

--- sql_file.sql
+++ sql_file.sql
@@ -1,13 +1,20 @@
 CREATE TABLE users (
-    user_id PRIMARY KEY AUTOINCREMENT;
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT;
     user_name TEXT NOT NULL;
     user_password TEXT NO NULL;
 );
 
 CREATE TABLE items (
-    item_id PRIMARY KEY AUTOINCREMENT;
+    item_id INTEGER PRIMARY KEY AUTOINCREMENT;
     item_name TEXT NOT NULL;
     item_type TEXT NOT NULL;
     content TEXT NO NULL;
-)
+);
+
+CREATE TABLE comment (
+    comment_id INTEGER PRIMARY KEY AUTOINCREMENT;
+    user_name TEXT NO NULL;
+    content TEXT NO NULL;
+
+);
 
