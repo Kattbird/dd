b1b6e35a918ef8df1a80cca1d5807f5245baa472
Kattbird <emirnedzhmiev@gmail.com>
2025-07-07 15:37:15 +0100

    Database inputed content

--- sql_file.sql
+++ sql_file.sql
@@ -36,13 +36,30 @@ CREATE TABLE item_comments (
     FOREIGN KEY(user_id) REFERENCES users(user_id)
 );
 
-
+-- help for frontend
 INSERT INTO users(user_name,user_password,mod) VALUES ('Dannyminer','Gribby','TRUE');
 INSERT INTO users(user_name,user_password,mod) VALUES ('SabaEnjoyer','a','TRUE');
 INSERT INTO users(user_name,user_password,mod) VALUES ('KaperDan','HWX','TRUE');
-
 INSERT INTO items(item_name,item_type,item_content) VALUES (title,type,content);
-
 SELECT item_name,item_content FROM items WHERE item_type='{}';
-
 SELECT item_type FROM items;
+
+-- Race
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Giant','Race','Eigheen percent chance');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Human','Race','Eigheen percent chance');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Fairy','Race','Eigheen percent chance');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Beastman','Race','fifteen percent chance');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Vampite','Race','fifteen percent chance - human sized - have ears - gray skin - cant fly - will burn in sun - slower healt regen during day - normal food is poison');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Pig','Race','zero point tree tree percent chance');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Goddes','Race','five percent chance - has two wings - human sized');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Demon','Race','five percent chance - runs on all fours - have horns - skin tone red or other - only allowed first stage - demon combat style');
+
+-- Magic
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Sunshine','Magic','Escanors magic');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Drole Dance','Magic','Doles magic');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Spirit Spear Chaestifol','Magic','King magic');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Take-over','Magic','Random magic');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Time','Magic','Dio magic');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Transpork','Magic','Hawks magic');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Light','Magic','Goddeses magic');
+INSERT INTO items(item_name,item_type,item_content) VALUES ('Full counter','Magic','Maliodas magic');
