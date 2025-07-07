6cd562b2ed08c5061103441dbccefd17364dd0cc
KaperDan <kaperutsu100@gmail.com>
2025-07-07 11:31:31 +0100

    o

--- app.py
+++ app.py
@@ -15,12 +15,8 @@ def main():
         conn = sqlite3.connect("database.db")
         cur = conn.cursor()
         mod = cur.execute(f"SELECT mod FROM users WHERE user_name='{session["username"]}'")
-<<<<<<< HEAD
-        return render_template("main.html", username=session["username"], logged_in=session["logged_in"], mod=mod, types=[('magic'), ('fighting styles'),])
-=======
         item_types = cur.execute("SELECT item_type FROM items;").fetchall()
         return render_template("main.html", username=session["username"], logged_in=session["logged_in"], mod=mod, types=item_types)
->>>>>>> 8c8b5cdefdc6bc8e1dd13696c446fe033b4ca60f
     else:
         return render_template("main.html", types=[('magic'), ('fighting styles'),])
 
@@ -125,19 +121,24 @@ def content_add_db():
             conn.commit()
             return redirect(url_for("main"))
     
-<<<<<<< HEAD
-@app.route("/types/<type>")
-def content():
-    return render_template("content.html")
-=======
-@app.route("/types/<type_chosen>")
-def content(type_chosen):
-    conn = sqlite3.connect("database.db")
-    cur = conn.cursor()
-    title = cur.execute(f"SELECT item_name FROM items WHERE item_type={type_chosen}")
-    content = cur.execute(f"SELECT item_content FROM items WHERE item_type={type_chosen}")
-    return render_template("content.html", type=type_chosen, title=title, content=content)
->>>>>>> 8c8b5cdefdc6bc8e1dd13696c446fe033b4ca60f
+@app.route("/fighting_styles")
+def fighting_style():
+    return render_template("fighting_styles.html")
+
+
+@app.route("/magic")
+def magic():
+    return render_template("magic.html")
+
+
+@app.route("/races")
+def races():
+    return render_template("races.html")
+
+
+@app.route("/skills")
+def skills():
+    return render_template("skills.html")
 
 if __name__ == "__main__":
     app.run()
