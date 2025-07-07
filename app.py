6f964305e26926cffd41c5c2d1c12bf33bd30a18
Daniel Morais <daniel13042008@gmail.com>
2025-07-07 13:58:53 +0100

    yes

--- app.py
+++ app.py
@@ -13,16 +13,9 @@ app.secret_key = os.getenv("key")
 def main():
     conn = sqlite3.connect("database.db")
     cur = conn.cursor()
-<<<<<<< HEAD
     item_types = cur.execute("SELECT item_type FROM items;").fetchall()
     if "logged_in" in session:
-        mod = cur.execute(f"SELECT mod FROM users WHERE user_name='{session["username"]}'")
-=======
-
-    item_types = cur.execute("SELECT item_type FROM items;").fetchall()
-    if "logged_in" in session:
-        mod = bool(cur.execute(f"SELECT mod FROM users WHERE user_name='{session["username"]}'"))
->>>>>>> b98b9bafa151c830d7a69df6f919523b640236a4
+        mod = bool(cur.execute("SELECT mod FROM users WHERE user_name='?}'", (session["username"],)))
         return render_template("main.html", username=session["username"], logged_in=session["logged_in"], mod=mod, types=item_types)
     else:
         return render_template("main.html", types=item_types)
@@ -31,67 +24,56 @@ def main():
 def login():
     return render_template("login.html")
 
-@app.route("/login_check", methods=["POST", "GET"])
+@app.route("/login", methods=["POST", "GET"])
 def login_check():
-    conn = sqlite3.connect("database.db")
-    cur = conn.cursor()
+    if request.method == "POST":
+        conn = sqlite3.connect("database.db")
+        cur = conn.cursor()
 
-    username = request.form.get("user")
-    password = request.form.get("pass")
+        username = request.form.get("user")
+        password = request.form.get("pass")
 
-    users = cur.execute("SELECT user_name, user_password FROM users;")
+        users = cur.execute("SELECT user_password FROM users WHERE user_name = ?;", (username,)).fetchall()
 
-    username_found = False
-    password_found = False
+        username_found = False
 
-    for i in users:
-        if i[0] == username:
-            username_found = True
-            if i[1] == password:
-                password_found = True
-            break
-    
-    if (username_found and password_found):
+        if not len(users) > 0:
+            username_found = False
+        
+        if (username_found):
 
-        session["username"] = username
-        session["logged_in"] = True
+            session["username"] = username
+            session["logged_in"] = True
 
-        return redirect(url_for("main"))
+            return redirect(url_for("main"))
     else:
-        return redirect(url_for("login"))
+        return render_template("login.html")
 
-@app.route("/signup")
-def signup():
-    return render_template("signup.html")
 
-@app.route("/signup_check", methods=["POST", "GET"])
+@app.route("/signup", methods=["POST", "GET"])
 def signup_check():
-    
-    conn = sqlite3.connect("database.db")
-    cur = conn.cursor()
+    if request.method == "POST":
+        conn = sqlite3.connect("database.db")
+        cur = conn.cursor()
 
-    username = request.form.get("user")
-    password = request.form.get("pass")
+        username = request.form.get("user")
+        password = request.form.get("pass")
 
-    users = cur.execute("SELECT user_name FROM users;")
+        users = cur.execute("SELECT user_name FROM users WHERE user_name = ?;", (username,)).fetchone()[0]
 
-    username_found = False
-
-    for i in users:
-        if i[0] == username:
+        if len(users) > 0:
             username_found = True
-            break
-    
-    if not username_found:
-        cur.execute(f"INSERT INTO users (user_name, user_password, mod) VALUES ('{username}', '{password}', FALSE);")
-        conn.commit()
+        
+        if username_found:
+            cur.execute("INSERT INTO users (user_name, user_password, mod) VALUES ('?', '?', FALSE);", (username, password,))
+            conn.commit()
 
-        session["username"] = username
-        session["logged_in"] = True
+            session["username"] = username
+            session["logged_in"] = True
 
-        return redirect(url_for("main"))
+            return redirect(url_for("main"))
     else:
-        return redirect(url_for("signup"))
+        return render_template("signup.html")
 
 @app.route("/logout")
 def logout():
@@ -105,7 +87,7 @@ def content_add():
     conn = sqlite3.connect("database.db")
     cur = conn.cursor()
     if "logged_in" in session:
-        mod = bool(cur.execute(f"SELECT mod FROM users WHERE user_name='{session["username"]}';").fetchone()[0])
+        mod = bool(cur.execute("SELECT mod FROM users WHERE user_name='?';", (session["username"],)).fetchone()[0])
         if mod:
             return render_template("content_add.html")
         else:
@@ -118,12 +100,12 @@ def content_add_db():
     conn = sqlite3.connect("database.db")
     cur = conn.cursor()
     if "logged_in" in session:
-        mod = bool(cur.execute(f"SELECT mod FROM users WHERE user_name='{session["username"]}';").fetchone())
+        mod = bool(cur.execute("SELECT mod FROM users WHERE user_name='?';", (session["username"],)).fetchone())
         if mod:
             title = request.form.get("title")
             type = request.form.get("type")
             content = request.form.get("content")
-            cur.execute(f"INSERT INTO items (item_name, item_type, item_content) VALUES ('{title}', '{type}', '{content}');")
+            cur.execute("INSERT INTO items (item_name, item_type, item_content) VALUES ('?', '?', '?');", (title, type, content),)
             conn.commit()
             return redirect(url_for("main"))
     
@@ -133,12 +115,8 @@ def content(type_chosen):
     type_chosen = type_chosen[0]
     conn = sqlite3.connect("database.db")
     cur = conn.cursor()
-    title = cur.execute(f"SELECT item_name FROM items WHERE item_type=('{type_chosen}');")
-    content = cur.execute(f"SELECT item_content FROM items WHERE item_type=('{type_chosen}');")
-<<<<<<< HEAD
-=======
-    print()
->>>>>>> b98b9bafa151c830d7a69df6f919523b640236a4
+    title = cur.execute("SELECT item_name FROM items WHERE item_type=?;", (type_chosen,))
+    content = cur.execute("SELECT item_content FROM items WHERE item_type=?;", (type_chosen,))
     return render_template("content.html", type=type_chosen, title=title, content=content)
 
 if __name__ == "__main__":
